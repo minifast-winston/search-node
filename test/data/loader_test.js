d2f60f88a8c748e8ddb006aee4cb9a2167960b2b
Doc Ritezel and Evan Tahler <pair+doc+evan@ministryofvelocity.com>
2016-10-06 17:18:08 -0700

    Add specs for Loader#create/#load
    * Currently requires a running elastic search instance

--- test/data/loader_test.js
+++ test/data/loader_test.js
@@ -1,5 +1,6 @@
 const should = require('should');
 const Loader = require('../../lib/loader').Loader;
+const Client = require('../../lib/client').Client;
 
 describe('Loader', () => {
   var loader;
@@ -11,15 +12,13 @@ describe('Loader', () => {
   describe('.mappings', () => {
     it('returns the mapping definitions', () => {
       Loader.mappings().should.deepEqual({
-        mappings: {
-          facility: {
-            properties: {
-              loc: {
-                type: 'geo_point',
-                fielddata: {
-                  format: 'compressed',
-                  precision: '1km'
-                }
+        facility: {
+          properties: {
+            loc: {
+              type: 'geo_point',
+              fielddata: {
+                format: 'compressed',
+                precision: '1km'
               }
             }
           }
@@ -52,5 +51,111 @@ describe('Loader', () => {
     });
   });
 
-  describe('#load')
+  describe('#create', () => {
+    var elasticsearch;
+
+    beforeEach((done) => {
+      var testEnv = {
+        "compose-for-elasticsearch": [{
+          "credentials": {
+            "db_type": "elastic_search",
+            "uri_direct_1": "http://localhost:9200",
+            "uri": "http://localhost:9200"
+          }
+        }]
+      };
+      var client = new Client(testEnv);
+      elasticsearch = client.configure();
+
+      elasticsearch.indices.exists({
+        index: Loader.index()
+      }, (error, response) => {
+        if (!response) { return done(); }
+        if (error) { return done(error); }
+        elasticsearch.indices.delete({
+          index: Loader.index(),
+          refresh: true,
+        }, done);
+      });
+    });
+
+    it('creates the proper index', (done) => {
+      elasticsearch.indices.exists({
+        index: Loader.index()
+      }, (error, response) => {
+        should.not.exist(error);
+        response.should.equal(false);
+        loader.create(elasticsearch, () => {
+          elasticsearch.indices.exists({
+            index: Loader.index()
+          }, (error, response) => {
+            should.not.exist(error);
+            response.should.equal(true);
+            done();
+          });
+        });
+      });
+    });
+
+    it('creates a facilities mapping on the index', (done) => {
+      loader.create(elasticsearch, () => {
+        elasticsearch.indices.getMapping({
+          index: Loader.index(),
+          type: 'facility'
+        }, (error, response) => {
+          should.not.exist(error);
+          response.should.deepEqual({
+            'test-kaiser-locations': {mappings: {facility: Loader.mappings().facility}}
+          });
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#load', () => {
+    var elasticsearch;
+
+    beforeEach((done) => {
+      var testEnv = {
+        "compose-for-elasticsearch": [{
+          "credentials": {
+            "db_type": "elastic_search",
+            "uri_direct_1": "http://localhost:9200",
+            "uri": "http://localhost:9200"
+          }
+        }]
+      };
+      var client = new Client(testEnv);
+      elasticsearch = client.configure();
+
+      elasticsearch.indices.exists({
+        index: Loader.index()
+      }, (error, response) => {
+        if (!response) { return done(); }
+        if (error) { return done(error); }
+        elasticsearch.indices.delete({
+          index: Loader.index(),
+          refresh: true,
+        }, () => {
+          loader.create(elasticsearch, done);
+        });
+      });
+    });
+
+    it('loads the fixture data', (done) => {
+      loader.load(elasticsearch, (error) => {
+        if (error) { return done(error); }
+        elasticsearch.indices.refresh({index: Loader.index()}, (error) => {
+          if (error) { return done(error); }
+          elasticsearch.search({query: {match: {official_name: 'Wildomar Medical Offices'}}}, (error, response) => {
+            if (error) { return done(error); }
+            response.hits.total.should.equal(1);
+            response.hits.hits[0]._source.official_name.should.equal('Wildomar Medical Offices');
+            done();
+          });
+        });
+      });
+    });
+  });
 });
